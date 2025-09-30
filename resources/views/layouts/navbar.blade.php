c0090a3ff89aac35ab298406830753f4dbe4b53a
sulthon abdul hakim <151292159+slabkim@users.noreply.github.com>
2025-09-30 17:18:51 +0700

    Tugas Pertemuan Controller-view

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,31 @@
+﻿<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <i class="bi bi-layers-fill me-2"></i>Sulthon
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->is('/') ? 'active' : '' }}" href="{{ url('/') }}">
+                        <i class="bi bi-house-fill me-1"></i> Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->is('user') ? 'active' : '' }}" href="{{ url('/user') }}">
+                        <i class="bi bi-people-fill me-1"></i> User
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->is('user/create') ? 'active' : '' }}"
+                        href="{{ url('/user/create') }}">
+                        <i class="bi bi-person-plus-fill me-1"></i> Tambah User
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
